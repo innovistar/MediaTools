d6d4bafe0571d927e78621dcc92d14d7e3277149
innovistar <45834254+innovistar@users.noreply.github.com>
2022-09-09 15:21:06 +0300

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+# from .models import Profile, Contact, Playlist, Video
+from .models import Video, Images, Documents
+# Register your models here.
+# admin.site.register(Profile)
+# admin.site.register(Contact)
+# admin.site.register(Playlist)
+admin.site.register(Video)
+admin.site.register(Images)
+admin.site.register(Documents)
---
+++ static/css/new.js
@@ -0,0 +1,20 @@
+const dropArea = document.querySelector(".drop_box"),
+  button = dropArea.querySelector("button"),
+  dragText = dropArea.querySelector("header"),
+  input = dropArea.querySelector("input");
+let file;
+var filename;
+
+button.onclick = () => {
+  input.click();
+};
+
+input.addEventListener("change", function (e) {
+  var fileName = e.target.files[0].name;
+  let filedata = `
+    <div class="form">
+    <h4>${fileName}</h4>
+    
+    </div>`;
+  dropArea.innerHTML = filedata;
+});
